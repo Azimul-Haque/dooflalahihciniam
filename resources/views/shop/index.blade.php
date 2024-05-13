0b8b5d8f63fc570f4fe1373217d8d2f8678c6638
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-14 02:08:57 +0600

    Commit Tue 05/14/2024  2:08:57.31 11674

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -58,7 +58,7 @@
   {{-- <div id="preloader">
       <div id="status">&nbsp;</div>
   </div> --}}
-  <section class="wow fadeIn content-top-margin">
+  <section class="wow fadeIn ">
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-sm-12">@include('partials._slider')</div>
