9794eca7cefc53106f42fce55aace1ca892426ec
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-14 02:37:45 +0600

    Commit Tue 05/14/2024  2:37:45.24 2870

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -106,7 +106,7 @@
                     <div class="col-md-4 col-sm-4 text-center">
                         <i class="icon-envelope small-icon black-text"></i>
                         <h6 class="margin-two no-margin-bottom">
-                            <a href="mailto:info@almanifood.com" class="black-text">info@almanifood.com</a>
+                            <a href="mailto:info@mainichihalalfood.com" class="black-text">info@mainichihalalfood.com</a>
                         </h6>
                     </div>
                     <!-- end email -->
