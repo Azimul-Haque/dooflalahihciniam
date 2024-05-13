be1c488643d3c663e89f666285dce19f7d1622e7
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-14 04:13:17 +0600

    Commit Tue 05/14/2024  4:13:17.32 23652

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -31,7 +31,7 @@
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="google-site-verification" content="hG6zSbHNlZzqpReVDwBBhXUimK0gdvg4YHC185Nl1-A" />
 
-    <meta name="og:image" content="{{ asset('images/slider/slide1.jpg?id=23423asdasd') }}">
+    <meta name="og:image" content="{{ asset('images/slider/slide1.jpg') }}">
 
     <!-- animation -->
     
