72da37bf22979db942242298b525b4c3e2e4175c
Azimul-Haque <orbachinujbuk@gmail.com>
2024-07-18 03:55:59 +0600

    Commit Thu 07/18/2024  3:55:59.41 8686

--- resources/views/partials/_ogani-nav.blade.php
+++ resources/views/partials/_ogani-nav.blade.php
@@ -7,7 +7,7 @@
     <div class="humberger__menu__overlay"></div>
     <div class="humberger__menu__wrapper">
         <div class="humberger__menu__logo">
-            <a href="#"><img src="{{ asset('vendor/ogani/img/logo.png') }}" alt=""></a>
+            <a href="#"><img src="{{ asset('images/logo.png') }}" alt=""></a>
         </div>
         <div class="humberger__menu__cart">
             <ul>
