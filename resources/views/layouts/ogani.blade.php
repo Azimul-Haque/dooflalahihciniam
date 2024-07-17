073e0db79d7e4f36bd8965db7f7b17b8afeb15a8
Azimul-Haque <orbachinujbuk@gmail.com>
2024-07-18 03:40:11 +0600

    Commit Thu 07/18/2024  3:40:11.29 13142

--- resources/views/layouts/ogani.blade.php
+++ resources/views/layouts/ogani.blade.php
@@ -165,14 +165,14 @@
     <!-- Footer Section End -->
 
     <!-- Js Plugins -->
-    <script src="js/jquery-3.3.1.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/jquery.nice-select.min.js"></script>
-    <script src="js/jquery-ui.min.js"></script>
-    <script src="js/jquery.slicknav.js"></script>
-    <script src="js/mixitup.min.js"></script>
-    <script src="js/owl.carousel.min.js"></script>
-    <script src="js/main.js"></script>
+    <script src="{{ asset('vendor/ogani/js/jquery-3.3.1.min.js') }}"></script>
+    <script src="{{ asset('vendor/ogani/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('vendor/ogani/js/jquery.nice-select.min.js') }}"></script>
+    <script src="{{ asset('vendor/ogani/js/jquery-ui.min.js') }}"></script>
+    <script src="{{ asset('vendor/ogani/js/jquery.slicknav.js') }}"></script>
+    <script src="{{ asset('vendor/ogani/js/mixitup.min.js') }}"></script>
+    <script src="{{ asset('vendor/ogani/js/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('vendor/ogani/js/main.js') }}"></script>
 
 
 
