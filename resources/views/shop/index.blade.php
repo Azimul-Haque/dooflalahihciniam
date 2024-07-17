eac5fb101d9587831bf33d5550415661296e7238
Azimul-Haque <orbachinujbuk@gmail.com>
2024-07-18 04:05:14 +0600

    Commit Thu 07/18/2024  4:05:14.26 12874

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -7,7 +7,41 @@
 @endsection
 
 @section('content')
-
+  <!-- Categories Section Begin -->
+  <section class="categories" style="margin-top: 30px;">
+      <div class="container">
+          <div class="row">
+              <div class="categories__slider owl-carousel">
+                  <div class="col-lg-3">
+                      <div class="categories__item set-bg" data-setbg="img/categories/cat-1.jpg">
+                          <h5><a href="#">Fresh Fruit</a></h5>
+                      </div>
+                  </div>
+                  <div class="col-lg-3">
+                      <div class="categories__item set-bg" data-setbg="img/categories/cat-2.jpg">
+                          <h5><a href="#">Dried Fruit</a></h5>
+                      </div>
+                  </div>
+                  <div class="col-lg-3">
+                      <div class="categories__item set-bg" data-setbg="img/categories/cat-3.jpg">
+                          <h5><a href="#">Vegetables</a></h5>
+                      </div>
+                  </div>
+                  <div class="col-lg-3">
+                      <div class="categories__item set-bg" data-setbg="img/categories/cat-4.jpg">
+                          <h5><a href="#">drink fruits</a></h5>
+                      </div>
+                  </div>
+                  <div class="col-lg-3">
+                      <div class="categories__item set-bg" data-setbg="img/categories/cat-5.jpg">
+                          <h5><a href="#">drink fruits</a></h5>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </section>
+  <!-- Categories Section End -->
 @endsection
 
 @section('js')
