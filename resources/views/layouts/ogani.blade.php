e1e93f1c7d5571f8a1d7dd0d389d00ac86a0bb53
Azimul-Haque <orbachinujbuk@gmail.com>
2024-07-18 03:41:12 +0600

    Commit Thu 07/18/2024  3:41:12.27 26542

--- resources/views/layouts/ogani.blade.php
+++ resources/views/layouts/ogani.blade.php
@@ -153,8 +153,9 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="footer__copyright">
-                        <div class="footer__copyright__text"><p>
-  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved</p></div>
+                        <div class="footer__copyright__text">
+                            <p>Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved</p>
+                        </div>
                         <div class="footer__copyright__payment"><img src="img/payment-item.png" alt=""></div>
                     </div>
                 </div>
