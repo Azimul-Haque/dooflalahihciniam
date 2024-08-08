a8f6fcc9588fd8b44531f8c64573962d20eb5b74
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-09 02:32:25 +0600

    Commit Fri 08/09/2024  2:32:25.24 16018

--- resources/views/partials/_ogani-nav.blade.php
+++ resources/views/partials/_ogani-nav.blade.php
@@ -33,7 +33,7 @@
         <nav class="humberger__menu__nav mobile-menu">
             <ul>
                 <li class="active"><a href="{{ route('product.index') }}">Home</a></li>
-                <li><a href="./shop-grid.html">Shop</a></li>
+                <li><a href="{{ route('product.index') }}">Shop</a></li>
                 <li><a href="#">Pages</a>
                     <ul class="header__menu__dropdown">
                         <li><a href="./shop-details.html">Shop Details</a></li>
