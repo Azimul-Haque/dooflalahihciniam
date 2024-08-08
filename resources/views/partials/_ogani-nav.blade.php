48a2e409566b4ec9114977493988cfe3ed80e12a
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-09 02:43:26 +0600

    Commit Fri 08/09/2024  2:43:26.23 15722

--- resources/views/partials/_ogani-nav.blade.php
+++ resources/views/partials/_ogani-nav.blade.php
@@ -33,7 +33,7 @@
         <nav class="humberger__menu__nav mobile-menu">
             <ul>
                 <li @if(Request::is('/')) class="active" @endif><a href="{{ route('product.index') }}">Home</a></li>
-                <li><a href="{{ route('product.shop') }}">Shop</a></li>
+                <li @if(Request::is('/shop')) class="active" @endif><a href="{{ route('product.shop') }}">Shop</a></li>
                 <li><a href="#">Pages</a>
                     <ul class="header__menu__dropdown">
                         <li><a href="./shop-details.html">Shop Details</a></li>
