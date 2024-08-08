2ffc52ef8b05b46997eb048bb4ecba1a77a320ee
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-09 03:32:14 +0600

    Commit Fri 08/09/2024  3:32:14.42 1997

--- resources/views/partials/_ogani-nav.blade.php
+++ resources/views/partials/_ogani-nav.blade.php
@@ -40,6 +40,11 @@
                         <li><a href="./shoping-cart.html">Shoping Cart</a></li>
                         <li><a href="./checkout.html">Check Out</a></li>
                         <li><a href="./blog-details.html">Blog Details</a></li>
+                    </ul><ul class="header__menu__dropdown">
+                        <li><a href="./shop-details.html">Shop Details</a></li>
+                        <li><a href="./shoping-cart.html">Shoping Cart</a></li>
+                        <li><a href="./checkout.html">Check Out</a></li>
+                        <li><a href="./blog-details.html">Blog Details</a></li>
                     </ul>
                 </li>
                 {{-- <li><a href="./blog.html">Blog</a></li> --}}
