b72617cf9b13269e0f3410a0278329dd4b2d5fda
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-09 03:51:54 +0600

    Commit Fri 08/09/2024  3:51:54.24 5933

--- resources/views/partials/_ogani-nav.blade.php
+++ resources/views/partials/_ogani-nav.blade.php
@@ -158,7 +158,7 @@
                                 </a>
                             </li>
                         </ul>
-                        <div class="header__cart__price"><a>¥150.00</a></div>
+                        <div class="header__cart__price"><a href="{{ route('product.shoppingcart') }}">¥150.00</a></div>
                     </div>
                 </div>
             </div>
