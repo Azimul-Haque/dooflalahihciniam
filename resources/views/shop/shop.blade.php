4b0924f3040d9718b4bd88856dae93a58f27a50d
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-09 02:52:27 +0600

    Commit Fri 08/09/2024  2:52:27.35 5710

---
+++ resources/views/shop/shop.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.ogani')
+
+@section('title', 'Halal Food Store in Japan')
+
+@section('css')
+
+@endsection
+
+@section('content')
+  
+@endsection
+
+@section('js')
+
+@endsection
