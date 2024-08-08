04594950531308e40192f897396bead4e7293735
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-09 02:34:56 +0600

    Commit Fri 08/09/2024  2:34:56.29 14080

--- resources/views/partials/_ogani-nav.blade.php
+++ resources/views/partials/_ogani-nav.blade.php
@@ -112,7 +112,7 @@
                     <nav class="header__menu">
                         <ul>
                             <li><a href="{{ route('product.index') }}">Home</a></li>
-                            <li class="active"><a href="{{ route('product.index') }}">Shop</a></li>
+                            <li class="active"><a href="{{ route('product.shop') }}">Shop</a></li>
                             <li><a href="#">Pages</a>
                                 <ul class="header__menu__dropdown">
                                     <li><a href="./shop-details.html">Shop Details</a></li>
