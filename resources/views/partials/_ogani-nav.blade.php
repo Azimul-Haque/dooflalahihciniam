b5496723f5fb15d0066833ec0305fdae62963dbc
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-09 03:35:52 +0600

    Commit Fri 08/09/2024  3:35:52.23 6440

--- resources/views/partials/_ogani-nav.blade.php
+++ resources/views/partials/_ogani-nav.blade.php
@@ -45,9 +45,9 @@
                 {{-- <li><a href="./blog.html">Blog</a></li> --}}
                 <li><a href="{{ route('index.contact') }}">Contact</a></li>
                 @if(Auth::user())
-                    <li @if(Request::is('categories')) class="active" @endif><a href="#">User</a>
+                    <li><a href="#">User</a>
                         <ul class="header__menu__dropdown">
-                            <li><a href="./shop-details.html">Shop Details</a></li>
+                            <li><a href="./shop-details.html">Profile</a></li>
                             <li><a href="./shoping-cart.html">Shoping Cart</a></li>
                             <li><a href="./checkout.html">Check Out</a></li>
                             <li><a href="./blog-details.html">Blog Details</a></li>
