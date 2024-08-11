524e436a3e6348a173e9a17a995831d35875caf6
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 05:07:39 +0600

    Commit Mon 08/12/2024  5:07:39.29 8870

--- resources/views/shop/singleproduct.blade.php
+++ resources/views/shop/singleproduct.blade.php
@@ -143,7 +143,7 @@
                       @if(Auth::check())
                         <a href="{{ route('product.addtowishlist', [$product->id, Auth::user()->id]) }}" class="heart-icon">
                           @if(in_array($product->id, Auth::user()->wishlists->pluck('product_id')->toArray()))
-                            <span class="icon_heart" style="color: red;"></span>
+                            <span class="icon_heart" style="color: #7FAD39;"></span>
                           @else
                             <span class="icon_heart_alt"></span>
                           @endif
