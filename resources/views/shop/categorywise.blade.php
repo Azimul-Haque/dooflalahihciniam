04b9da421e1591ca478da9846a2809d71901cc2c
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 03:59:46 +0600

    Commit Mon 08/12/2024  3:59:46.33 24180

--- resources/views/shop/categorywise.blade.php
+++ resources/views/shop/categorywise.blade.php
@@ -164,7 +164,7 @@
                             </div>
                             <div class="col-lg-4 col-md-4">
                                 <div class="filter__found">
-                                    <h6><span>16</span> Products found</h6>
+                                    <h6><span>{{ $products->count() }}</span> Products found</h6>
                                 </div>
                             </div>
                             <div class="col-lg-4 col-md-3">
