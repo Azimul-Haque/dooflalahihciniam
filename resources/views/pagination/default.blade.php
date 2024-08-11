2fd4a8e7b847e0d64c69a615c30b8940d2655dfa
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 03:12:26 +0600

    Commit Mon 08/12/2024  3:12:26.27 20878

--- resources/views/pagination/default.blade.php
+++ resources/views/pagination/default.blade.php
@@ -4,7 +4,7 @@
 
 @if ($paginator->lastPage() > 1)
 <div class="product__pagination">
-    <a href="#">1</a>
+    <a href="{{ $paginator->url(1) }}">1</a>
     <a href="#">2</a>
     <a href="#">3</a>
     <a href="#"><i class="fa fa-long-arrow-right"></i></a>
