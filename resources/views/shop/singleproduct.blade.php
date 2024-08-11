7a0806af3ff0134ddf16c3939cf05830947360bc
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 04:28:43 +0600

    Commit Mon 08/12/2024  4:28:43.29 17435

--- resources/views/shop/singleproduct.blade.php
+++ resources/views/shop/singleproduct.blade.php
@@ -130,7 +130,7 @@
                           @endif
                           <span>({{ $product->productreviews->count() }} reviews)</span>
                       </div>
-                      <div class="product__details__price">¥{{ $product->price }} <small>¥{{ $product->price }}</small></div>
+                      <div class="product__details__price">¥{{ $product->price }} <small>¥{{ $product->oldprice }}</small></div>
                       <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vestibulum ac diam sit amet quam
                           vehicula elementum sed sit amet dui. Sed porttitor lectus nibh. Vestibulum ac diam sit amet
                           quam vehicula elementum sed sit amet dui. Proin eget tortor risus.</p>
