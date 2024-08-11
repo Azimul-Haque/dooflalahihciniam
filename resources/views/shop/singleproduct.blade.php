383cab677fa6972f4fd5bfca6738281a9a17a40a
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 04:49:14 +0600

    Commit Mon 08/12/2024  4:49:14.31 1321

--- resources/views/shop/singleproduct.blade.php
+++ resources/views/shop/singleproduct.blade.php
@@ -267,7 +267,7 @@
                           </div>
                           <div class="tab-pane" id="tabs-3" role="tabpanel">
                               <div class="product__details__tab__desc">
-                                  <h6>Products Infomation</h6>
+                                  <h6>Facebook Comments</h6>
                                   <p>Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.
                                       Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus.
                                       Vivamus suscipit tortor eget felis porttitor volutpat. Vestibulum ac diam
