79486ff1ff94651cf8ae58bea66567f1b046c00d
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 02:05:38 +0600

    Commit Mon 08/12/2024  2:05:38.22 22295

--- resources/views/shop/shop.blade.php
+++ resources/views/shop/shop.blade.php
@@ -176,21 +176,24 @@
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 col-6">
-                            <div class="product__item">
-                                <div class="product__item__pic set-bg" data-setbg="{{ asset('vendor/ogani/img/product/product-1.jpg') }}">
-                                    <ul class="product__item__pic__hover">
-                                        {{-- <li><a href="#"><i class="fa fa-heart"></i></a></li>
-                                        <li><a href="#"><i class="fa fa-retweet"></i></a></li> --}}
-                                        <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-                                    </ul>
-                                </div>
-                                <div class="product__item__text">
-                                    <h6><a href="#">Crab Pool Security</a></h6>
-                                    <h5>¥30.00</h5>
-                                </div>
-                            </div>
-                        </div>
+                        @foreach($products as $product)
+                          <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 col-6">
+                              <div class="product__item">
+                                  <div class="product__item__pic set-bg" data-setbg="{{ asset('vendor/ogani/img/product/product-1.jpg') }}">
+                                      <ul class="product__item__pic__hover">
+                                          {{-- <li><a href="#"><i class="fa fa-heart"></i></a></li>
+                                          <li><a href="#"><i class="fa fa-retweet"></i></a></li> --}}
+                                          <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
+                                      </ul>
+                                  </div>
+                                  <div class="product__item__text">
+                                      <h6><a href="#">Crab Pool Security</a></h6>
+                                      <h5>¥30.00</h5>
+                                  </div>
+                              </div>
+                          </div>
+                        @endforeach
+                        
                         
                         <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 col-6">
                             <div class="product__item">
