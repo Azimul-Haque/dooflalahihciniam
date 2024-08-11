fe9994ad064d167e30fd7ec2d97d6592f2ace3bb
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 04:24:25 +0600

    Commit Mon 08/12/2024  4:24:25.23 3159

--- resources/views/shop/singleproduct.blade.php
+++ resources/views/shop/singleproduct.blade.php
@@ -91,7 +91,7 @@
               </div>
               <div class="col-lg-6 col-md-6">
                   <div class="product__details__text">
-                      <h3>Vetgetable’s Package</h3>
+                      <h3>{{ $product->title }}</h3>
                       <div class="product__details__rating">
                           <i class="fa fa-star"></i>
                           <i class="fa fa-star"></i>
