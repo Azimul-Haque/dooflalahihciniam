2df988ffe551d2bef4eda81ae56bb36c54a86dbd
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 01:43:47 +0600

    Commit Mon 08/12/2024  1:43:47.43 25025

--- resources/views/partials/_ogani-shop-left-menu.blade.php
+++ resources/views/partials/_ogani-shop-left-menu.blade.php
@@ -5,8 +5,8 @@
             @foreach($categories as $category)
                 @php
                     $text = $category->name;
-                    // $slug = \Illuminate\Support\Str::slug($text);
-                    $slug = preg_replace('/[^\w\d]+/', '-', $text);
+                    $slug = \Illuminate\Support\Str::slug(preg_replace('/[^\w\d]+/', '-', $text));
+                    // $slug = preg_replace('/[^\w\d]+/', '-', $text);
                 @endphp
                 <li><a href="{{ route('product.categorywise', [$category->id, $slug]) }}">{{ $category->name }}</a></li>
             @endforeach
