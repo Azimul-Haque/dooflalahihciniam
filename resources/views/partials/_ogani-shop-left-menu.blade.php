ae37f3c1a9794528a2e900e62c9a3b03e736d016
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 02:48:18 +0600

    Commit Mon 08/12/2024  2:48:18.24 15948

--- resources/views/partials/_ogani-shop-left-menu.blade.php
+++ resources/views/partials/_ogani-shop-left-menu.blade.php
@@ -27,7 +27,7 @@
                             <a href="{{ route('product.categorywise', [$category->id, generate_token(100)]) }}">
                                 {{ $category->name }}<span>///{{ $category->products->count() }}</span>
                             </a>
-                            <ul style="margin-right: 20px;">
+                            <ul style="margin-left: 20px;">
                                 @foreach($category->subcategories as $subcategory)
                                     @php
                                       $totalproductofthissubcat = 0;
