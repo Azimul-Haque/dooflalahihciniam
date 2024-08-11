271c63845a0ee2bb6f25e789f32c4a528dd64953
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 01:38:35 +0600

    Commit Mon 08/12/2024  1:38:35.30 3830

--- resources/views/partials/_ogani-nav.blade.php
+++ resources/views/partials/_ogani-nav.blade.php
@@ -232,10 +232,9 @@
                         <ul>
                             @foreach($categories as $category)
                                 @php
-                                    use Illuminate\Support\Str;
-
+                                    
                                     $text = $category->name;
-                                    $slug = Str::slug($text);
+                                    $slug = \Illuminate\Support\Str::slug($text);
                                 @endphp
                                 <li><a href="{{ route('product.categorywise', [$category->id, $slug]) }}">{{ $category->name }}</a></li>
                             @endforeach
