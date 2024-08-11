9be9a70308e8d1f99710cd1ec6a05efab6b04619
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 02:05:59 +0600

    Commit Mon 08/12/2024  2:05:59.29 834

--- resources/views/shop/shop.blade.php
+++ resources/views/shop/shop.blade.php
@@ -187,7 +187,7 @@
                                       </ul>
                                   </div>
                                   <div class="product__item__text">
-                                      <h6><a href="#">Crab Pool Security</a></h6>
+                                      <h6><a href="#">{{ $product->title }}</a></h6>
                                       <h5>¥30.00</h5>
                                   </div>
                               </div>
