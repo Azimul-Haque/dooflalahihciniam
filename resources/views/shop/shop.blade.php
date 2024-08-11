da32f3595792d982c9c922475c8ae9ee51457cfa
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 03:57:55 +0600

    Commit Mon 08/12/2024  3:57:55.29 13523

--- resources/views/shop/shop.blade.php
+++ resources/views/shop/shop.blade.php
@@ -179,7 +179,7 @@
                         @foreach($products as $product)
                           <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 col-6">
                               <div class="product__item">
-                                  <div class="product__item__pic set-bg" data-setbg="{{ asset('vendor/ogani/img/product/product-1.jpg') }}">
+                                  <div class="product__item__pic set-bg" data-setbg="{{ asset('images/product-images/'.$product->productimages->first()->image) }}">
                                       <ul class="product__item__pic__hover">
                                           {{-- <li><a href="#"><i class="fa fa-heart"></i></a></li>
                                           <li><a href="#"><i class="fa fa-retweet"></i></a></li> --}}
