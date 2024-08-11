c621b7442cffeddef0752f073410df5fc0ad6122
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 05:12:31 +0600

    Commit Mon 08/12/2024  5:12:31.28 7368

--- resources/views/shop/singleproduct.blade.php
+++ resources/views/shop/singleproduct.blade.php
@@ -144,7 +144,7 @@
                               </div>
                           </div>
                       </div>
-                      <button class="primary-btn" id="addToCartSingle">ADD TO CARD</button>
+                      <a href="#!" class="primary-btn" id="addToCartSingle">ADD TO CARD</a>
                       @if(Auth::check())
                         <a href="{{ route('product.addtowishlist', [$product->id, Auth::user()->id]) }}" class="heart-icon">
                           @if(in_array($product->id, Auth::user()->wishlists->pluck('product_id')->toArray()))
