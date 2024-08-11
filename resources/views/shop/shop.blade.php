c15c14921970e8a26842f3de1000b734eac7c636
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 01:56:21 +0600

    Commit Mon 08/12/2024  1:56:21.25 7484

--- resources/views/shop/shop.blade.php
+++ resources/views/shop/shop.blade.php
@@ -40,7 +40,7 @@
                         </div>
                         <div class="row">
                             <div class="product__discount__slider owl-carousel">
-                                <div class="col-lg-3">
+                                <div class="col-lg-4">
                                     <div class="product__discount__item">
                                         <div class="product__discount__item__pic set-bg"
                                             data-setbg="{{ asset('vendor/ogani/img/product/discount/pd-1.jpg') }}">
@@ -58,7 +58,7 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-lg-3">
+                                <div class="col-lg-4">
                                     <div class="product__discount__item">
                                         <div class="product__discount__item__pic set-bg"
                                             data-setbg="{{ asset('vendor/ogani/img/product/discount/pd-2.jpg') }}">
@@ -76,7 +76,7 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-lg-3">
+                                <div class="col-lg-4">
                                     <div class="product__discount__item">
                                         <div class="product__discount__item__pic set-bg"
                                             data-setbg="{{ asset('vendor/ogani/img/product/discount/pd-3.jpg') }}">
