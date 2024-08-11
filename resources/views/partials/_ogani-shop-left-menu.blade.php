576d4584b7d5e5d9be7deb17d4396c360440bee8
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 02:38:19 +0600

    Commit Mon 08/12/2024  2:38:19.30 6279

--- resources/views/partials/_ogani-shop-left-menu.blade.php
+++ resources/views/partials/_ogani-shop-left-menu.blade.php
@@ -133,25 +133,6 @@
                           </div>
                       </a>
                     @endforeach
-                    
-                    <a href="#" class="latest-product__item">
-                        <div class="latest-product__item__pic">
-                            <img src="{{ asset('vendor/ogani/img/latest-product/lp-2.jpg') }}" alt="">
-                        </div>
-                        <div class="latest-product__item__text">
-                            <h6>Crab Pool Security</h6>
-                            <span>¥30.00</span>
-                        </div>
-                    </a>
-                    <a href="#" class="latest-product__item">
-                        <div class="latest-product__item__pic">
-                            <img src="{{ asset('vendor/ogani/img/latest-product/lp-3.jpg') }}" alt="">
-                        </div>
-                        <div class="latest-product__item__text">
-                            <h6>Crab Pool Security</h6>
-                            <span>¥30.00</span>
-                        </div>
-                    </a>
                 </div>
                 <div class="latest-prdouct__slider__item">
                     <a href="#" class="latest-product__item">
