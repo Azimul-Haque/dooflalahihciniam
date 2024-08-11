af0e461786a0b1741c8e08c3ed5e1564adae422b
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 03:15:23 +0600

    Commit Mon 08/12/2024  3:15:23.32 29358

--- resources/views/pagination/default.blade.php
+++ resources/views/pagination/default.blade.php
@@ -8,7 +8,7 @@
     <a href="#">2</a>
     <a href="#">3</a>
     <a href="#"><i class="fa fa-long-arrow-right"></i></a>
-    <a href="{{ $paginator->url($paginator->currentPage()+1) }}"></a>
+    <a href="{{ $paginator->url($paginator->currentPage()+1) }}"><i class="fa fa-long-arrow-right"></i></a>
 </div>
 
 <div class="pagination">
