6b061011237fae88c7617d65516553c230207d17
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 05:21:23 +0600

    Commit Mon 08/12/2024  5:21:23.31 14363

--- resources/views/shop/singleproduct.blade.php
+++ resources/views/shop/singleproduct.blade.php
@@ -86,7 +86,9 @@
                               src="{{ asset('images/product-images/' . $image->image) }}" alt="{{ $product->title }}">
                         @endforeach
                           <img data-imgbigurl="{{ asset('vendor/ogani/img/product/details/product-details-3.jpg') }}"
-                              src="{{ asset('vendor/ogani/img/product/details/product-details-2.jpg') }}" alt="">
+                              src="{{ asset('vendor/ogani/img/product/details/product-details-3.jpg') }}" alt="">
+                              <img data-imgbigurl="{{ asset('vendor/ogani/img/product/details/product-details-4.jpg') }}"
+                              src="{{ asset('vendor/ogani/img/product/details/product-details-4.jpg') }}" alt="">
                           {{-- <img data-imgbigurl="img/product/details/product-details-5.jpg"
                               src="img/product/details/thumb-3.jpg" alt="">
                           <img data-imgbigurl="img/product/details/product-details-4.jpg"
