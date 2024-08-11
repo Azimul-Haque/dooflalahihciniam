a68bbd5f2dc89d140445433f21ec83105ff8436d
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 05:00:31 +0600

    Commit Mon 08/12/2024  5:00:31.32 25927

--- resources/views/shop/singleproduct.blade.php
+++ resources/views/shop/singleproduct.blade.php
@@ -141,7 +141,6 @@
                       </div>
                       <a href="#" class="primary-btn">ADD TO CARD</a>
                       @if(Auth::check())
-                        <a href="" class="text-uppercase text-small vertical-align-middle"><i class="fa fa-heart-o black-text"></i> Add to wishlist</a>
                         <a href="{{ route('product.addtowishlist', [$product->id, Auth::user()->id]) }}" class="heart-icon"><span class="icon_heart_alt"></span></a>
                       @else
                         <a title="You need to Login to Add this product in your WishList" href="{{ url('login') }}" class="text-uppercase text-small vertical-align-middle"><i class="fa fa-heart-o black-text"></i> Add to wishlist</a>
