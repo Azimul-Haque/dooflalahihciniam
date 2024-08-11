1b01b17e1d311ef76d3ad6822efdb64c252551ec
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 02:39:26 +0600

    Commit Mon 08/12/2024  2:39:26.35 4529

--- resources/views/partials/_ogani-shop-left-menu.blade.php
+++ resources/views/partials/_ogani-shop-left-menu.blade.php
@@ -148,7 +148,7 @@
                               <h6>{{ $product->title }}</h6>
                               <span>
                                   @if($product->oldprice > 0)
-                                    <del>¥ {{ $product->oldprice }}</del>
+                                    <small><del>¥ {{ $product->oldprice }}</del></small>
                                   @endif
                                   ¥ {{ $product->price }}
                               </span>
