ee33379e568b02cd87451b10e77ebed03534ac99
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 04:17:13 +0600

    Commit Mon 08/12/2024  4:17:13.33 10143

--- resources/views/shop/singleproduct.blade.php
+++ resources/views/shop/singleproduct.blade.php
@@ -48,16 +48,15 @@
 @section('content')
   
   <!-- Breadcrumb Section Begin -->
-  <section class="breadcrumb-section set-bg" data-setbg="img/breadcrumb.jpg">
+  <section class="breadcrumb-section set-bg" data-setbg="{{ asset('vendor/ogani/img/breadcrumb.jpg') }}">
       <div class="container">
           <div class="row">
               <div class="col-lg-12 text-center">
                   <div class="breadcrumb__text">
-                      <h2>Vegetable’s Package</h2>
+                      <h2>Organi Shop</h2>
                       <div class="breadcrumb__option">
-                          <a href="./index.html">Home</a>
-                          <a href="./index.html">Vegetables</a>
-                          <span>Vegetable’s Package</span>
+                          <a href="{{ route('product.index') }}">Home</a>
+                          <span>Shop</span>
                       </div>
                   </div>
               </div>
