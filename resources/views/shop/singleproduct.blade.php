c6152fe4ad0899f2414607ee34d3713798054e6a
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 05:04:43 +0600

    Commit Mon 08/12/2024  5:04:43.26 8917

--- resources/views/shop/singleproduct.blade.php
+++ resources/views/shop/singleproduct.blade.php
@@ -143,7 +143,7 @@
                       @if(Auth::check())
                         <a href="{{ route('product.addtowishlist', [$product->id, Auth::user()->id]) }}" class="heart-icon">
                           @php
-                            echo Auth::user()->wishlists->pluck('id');
+                            if(in_array(needle, Auth::user()->wishlists->pluck('id')->toArray));
                           @endphp
                           <span class="icon_heart_alt"></span>
                         </a>
