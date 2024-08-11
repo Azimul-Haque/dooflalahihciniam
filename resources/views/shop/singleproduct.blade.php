77419fa5514c994f12aee401641ed3e700d28e19
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 04:20:02 +0600

    Commit Mon 08/12/2024  4:20:02.27 20289

--- resources/views/shop/singleproduct.blade.php
+++ resources/views/shop/singleproduct.blade.php
@@ -55,8 +55,8 @@
                   <div class="breadcrumb__text">
                       {{-- <h2>Organi Shop</h2> --}}
                       <div class="breadcrumb__option">
-                        
-                        <a href="{{ route('product.index') }}">{{ $product->title }}</a><span> Product</span>
+                        <a href="{{ route('product.index') }}">{{ $product->title }}</a>
+                        <span>Product</span>
                       </div>
                   </div>
               </div>
