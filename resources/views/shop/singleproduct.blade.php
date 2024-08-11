b9dfef9a7c9cde84ffa0c0d5300a77f8089b4f12
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 05:20:21 +0600

    Commit Mon 08/12/2024  5:20:21.25 11042

--- resources/views/shop/singleproduct.blade.php
+++ resources/views/shop/singleproduct.blade.php
@@ -85,7 +85,7 @@
                           <img data-imgbigurl="{{ asset('images/product-images/' . $image->image) }}"
                               src="{{ asset('images/product-images/' . $image->image) }}" alt="{{ $product->title }}">
                         @endforeach
-                          <img data-imgbigurl="{{ asset('img/product/details/product-details-3.jpg') }}"
+                          <img data-imgbigurl="{{ asset('vendor/ogani/img/product/details/product-details-3.jpg') }}"
                               src="img/product/details/thumb-2.jpg" alt="">
                           {{-- <img data-imgbigurl="img/product/details/product-details-5.jpg"
                               src="img/product/details/thumb-3.jpg" alt="">
