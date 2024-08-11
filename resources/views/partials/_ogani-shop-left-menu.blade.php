7d6227053e336d661216b115d50760c00805f70d
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 01:32:26 +0600

    Commit Mon 08/12/2024  1:32:26.37 14947

--- resources/views/partials/_ogani-shop-left-menu.blade.php
+++ resources/views/partials/_ogani-shop-left-menu.blade.php
@@ -2,6 +2,9 @@
     <div class="sidebar__item">
         <h4>Categories</h4>
         <ul>
+            @foreach($categories as $category)
+                <li><a href="{{ route('product.categorywise', [$category->id, generate_token(50)]) }}">{{ $category->name }}</a></li>
+            @endforeach
             <li><a href="#">Fresh Meat</a></li>
             <li><a href="#">Vegetables</a></li>
             <li><a href="#">Fruit & Nut Gifts</a></li>
