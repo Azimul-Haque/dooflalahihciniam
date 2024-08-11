66e16cd4ebfeade7b421281cee5008f8b56b5104
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 03:23:10 +0600

    Commit Mon 08/12/2024  3:23:10.35 3368

--- resources/views/pagination/default.blade.php
+++ resources/views/pagination/default.blade.php
@@ -4,7 +4,7 @@
 
 @if ($paginator->lastPage() > 1)
 <div class="product__pagination">
-    <a href="{{ $paginator->url(1) }}" {{ ($paginator->currentPage() == 1) ? style="pointer-events: none" : '' }}><i class="fa fa-long-arrow-left"></i></a>
+    <a href="{{ $paginator->url(1) }}" {{ ($paginator->currentPage() == 1) ? 'style="pointer-events: none"' : '' }}><i class="fa fa-long-arrow-left"></i></a>
     @for ($i = 1; $i <= $paginator->lastPage(); $i++)
         @php
             $half_total_links = floor($link_limit / 2);
