a49deaf48a06e575f0d5681902cc88e1e83e49b1
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 02:35:22 +0600

    Commit Mon 08/12/2024  2:35:22.25 5060

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -46,7 +46,7 @@ class ProductController extends Controller
                          ->orderBy('id', 'desc')
                          ->take(50)
                          ->paginate(10);
-      $newarrivals = Product::orderBy('id', 'desc')->where('isAvailable', 1)->get()->take(5);
+      $newarrivals = Product::orderBy('id', 'desc')->where('isAvailable', 1)->get()->take(6);
 
       return view('shop.shop')
                   ->withProducts($products);
