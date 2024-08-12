56649992ea85096301aafeabce38395a23403f83
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 23:40:40 +0600

    Commit Mon 08/12/2024 23:40:40.25 8010

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -21,7 +21,7 @@
                       </div>
                   </div>
                   <div class="col-lg-3">
-                      <div class="categories__item set-bg" data-setbg="{{ asset('vendor/ogani/img/categories/cat-2.jpg') }}">
+                      <div class="categories__item set-bg" data-setbg="{{ asset('vendor/ogani/img/categories/cat-5.jpg') }}">
                           <h5><a href="#">Beef Item</a></h5>
                       </div>
                   </div>
