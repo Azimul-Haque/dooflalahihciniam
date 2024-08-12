76a1e88646c3627250c4e8f0ca546cab704328cc
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 22:29:25 +0600

    Commit Mon 08/12/2024 22:29:25.23 10571

--- resources/views/auth/emails/password.blade.php
+++ resources/views/auth/emails/password.blade.php
@@ -18,7 +18,7 @@
 	        This is a auto-generated email from <b>Mainichi Halal Food</b>. This email arrived to you because you (or may be someone else!) have requested to reset the password associated with this email address. If you are getting this email by mistake, please ignore it.
 	      </p>
 	      <p style="font-size: 12px; color: #ACACAC;">
-	        &copy; @php echo date('Y'); @endphp <a href="http://loyalovijatri.com/"><b>Mainichi</b> Halal Food</a>, Japan
+	        &copy; @php echo date('Y'); @endphp <a href="http://loyalovijatri.com/"><b>Mainichi</b> Halal Food</a>, Japan.
 	      </p>
 	    </center>
 	  </div>
