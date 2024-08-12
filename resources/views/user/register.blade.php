3a7f03a8b9e4bd555a4cfd366a26d2400d7c8daf
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 21:32:12 +0600

    Commit Mon 08/12/2024 21:32:12.29 17133

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -64,17 +64,19 @@
                   <div class="col-md-6">
                       {!! Form::label('phone', 'Phone No') !!}
                       {!! Form::text('phone', null, array('class' => 'form-control', 'required' => '', "onkeypress" => "if(this.value.length==11) return false;")) !!}{{-- onkeypress="if(this.value.length==11) return false;" --}}
+
+                      {!! Form::label('password', 'Password') !!}
+                      {!! Form::password('password', array('class' => 'form-control', 'required' => '')) !!}
+                      
+                      {!! Form::label('password_confirmation', 'Confirm Password') !!}
+                      {!! Form::password('password_confirmation' , array('class' => 'form-control', 'required' => '')) !!}
                   </div>
                   <div class="col-md-6">
                       {!! Form::label('address', 'Delivery Address') !!}
                       {!! Form::textarea('address', null, array('class' => 'form-control address', 'required' => '')) !!}
                   </div>
                   <div class="col-md-6">                  
-                      {!! Form::label('password', 'Password') !!}
-                      {!! Form::password('password', array('class' => 'form-control', 'required' => '')) !!}
-                  
-                      {!! Form::label('password_confirmation', 'Confirm Password') !!}
-                      {!! Form::password('password_confirmation' , array('class' => 'form-control', 'required' => '')) !!}
+                      
                   
                       {!! Form::label('captcha', 'Captcha') !!}
                       {!! app('captcha')->display() !!}
