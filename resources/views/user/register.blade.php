277b7795088f2b28ff42cd0c93ac6ff074de0a6a
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 21:28:53 +0600

    Commit Mon 08/12/2024 21:28:53.27 31558

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -56,10 +56,12 @@
                   <div class="col-md-6">
                       {!! Form::label('name', 'Name') !!}
                       {!! Form::text('name', null, array('class' => 'form-control', 'required' => '')) !!}
+                  </div>
                   <div class="col-md-6">
                       {!! Form::label('email', 'Email') !!}
                       {!! Form::text('email', null, array('class' => 'form-control', 'required' => '')) !!}
-
+                  </div>
+                  <div class="col-md-6">
                       {!! Form::label('password', 'Password') !!}
                       {!! Form::password('password', array('class' => 'form-control', 'required' => '')) !!}
 
