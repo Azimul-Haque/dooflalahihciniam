ea09831ca75d09b1f5bcaea7fdeba8f3039ec46e
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 21:21:36 +0600

    Commit Mon 08/12/2024 21:21:36.28 26055

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -47,7 +47,7 @@
   <section class="content-section padding-three">
       <div class="container">
         <div class="row">
-          <div class="col-md-4 mx-auto">
+          <div class="col-md-6 mx-auto">
             <div class="login-box">
               <h1 style="text-align: center">Register</h1>
               {!! Form::open(['route' => 'user.register', 'method' => 'POST']) !!}
