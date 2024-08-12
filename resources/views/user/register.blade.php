2f9c947785b075bbf7e419261d116a5ea9fc4410
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 21:29:10 +0600

    Commit Mon 08/12/2024 21:29:10.32 21872

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -68,7 +68,8 @@
                   <div class="col-md-6">
                       {!! Form::label('password_confirmation', 'Confirm Password') !!}
                       {!! Form::password('password_confirmation' , array('class' => 'form-control', 'required' => '')) !!}
-
+                  </div>
+                  <div class="col-md-6">
                       {!! Form::label('captcha', 'Captcha') !!}
                       {!! app('captcha')->display() !!}
                   </div>
@@ -76,6 +77,8 @@
                   <div class="col-md-6">
                       {!! Form::label('phone', 'Phone No') !!}
                       {!! Form::text('phone', null, array('class' => 'form-control', 'required' => '', "onkeypress" => "if(this.value.length==11) return false;")) !!}{{-- onkeypress="if(this.value.length==11) return false;" --}}
+                  </div>
+                  <div class="col-md-6">
                       {!! Form::label('address', 'Delivery Address') !!}
                       {!! Form::textarea('address', null, array('class' => 'form-control address', 'required' => '')) !!}
 
