6df9b35fe611392eed5a4c0dca2a23455441d5c2
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 21:07:25 +0600

    Commit Mon 08/12/2024 21:07:25.30 13894

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -5,8 +5,7 @@
 @section('css')
 <style type="text/css">
   .login-box {
-    padding-top: 40px;
-    padding-bottom: 60px;
+    
     -webkit-box-shadow: 3px 3px 10px 3px rgb(166 166 166 / 40%);
     -moz-box-shadow: 3px 3px 10px 3px rgba(166,166,166,0.4);
     box-shadow: 3px 3px 10px 3px rgb(166 166 166 / 40%);
