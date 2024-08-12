23b2490e6bfdbce935258812571ebd57d7a0f392
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 21:16:40 +0600

    Commit Mon 08/12/2024 21:16:40.24 16907

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -57,12 +57,10 @@
                   <label for="password">Password</label>
                   <input class="form-control" type="password" id="password" name="password">
                 </div>
-                <div style="float: left; margin-top: 5px;">
+                <div style="margin-top: 5px;">
                   <a href="{{ route('user.register') }}">Register</a> | <a href="{{ url(config('adminlte.password_reset_url', 'password/reset')) }}">Forgot password?</a>
                 </div>
-                <div style="float: right;">
-                  <a href="#!" type="submit" class="primary-btn">Login</a>
-                </div><br/><br/>
+                <a href="#!" type="submit" class="primary-btn">Login</a>
               </form>
               
             </div>
