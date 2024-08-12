d0e71682da68e3919286cf9c7053b4c3a1b8a3fd
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 23:40:56 +0600

    Commit Mon 08/12/2024 23:40:56.27 25622

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -22,7 +22,7 @@
                   </div>
                   <div class="col-lg-3">
                       <div class="categories__item set-bg" data-setbg="{{ asset('vendor/ogani/img/categories/cat-5.jpg') }}">
-                          <h5><a href="{{ Request::url() . '/category/10/canned-dried' }}">Beef Item</a></h5>
+                          <h5><a href="{{ Request::url() . '/category/1/beef' }}">Beef Item</a></h5>
                       </div>
                   </div>
                   <div class="col-lg-3">
