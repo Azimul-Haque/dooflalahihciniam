36aa1bfe6f5b9213e0da903e19579b6ef03140c8
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 20:47:31 +0600

    Commit Mon 08/12/2024 20:47:31.52 7716

--- resources/views/partials/_ogani-nav.blade.php
+++ resources/views/partials/_ogani-nav.blade.php
@@ -16,9 +16,10 @@
                 <li><a href="#"><i class="fa fa-heart"></i> <span>1</span></a></li>
                 <li>
                     <a href="{{ route('product.shoppingcart') }}"><i class="fa fa-shopping-bag"></i>
-                        @if(Session::has('cart'))
-                            <span id="totalInBagMobile">{{ Session::get('cart')->totalQty }}</span>
-                        @endif
+                        <span id="totalInBagMobile">
+                            {{ Session::get('cart')->totalQty }}
+                        </span>
+                        
                     </a>
                 </li>
             </ul>
