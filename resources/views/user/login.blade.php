4ac3e17804c76cbf20bfb4e9f9db3cd071547f18
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 21:15:53 +0600

    Commit Mon 08/12/2024 21:15:53.28 30071

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -60,8 +60,8 @@
                 <div style="float: left; margin-top: 5px;">
                   <a href="{{ route('user.register') }}">Register</a> | <a href="{{ url(config('adminlte.password_reset_url', 'password/reset')) }}">Forgot password?</a>
                 </div>
-                <div>
-                  <a href="#!" type="submit" class="primary-btn" style="float: right;">Login</a>
+                <div style="float: right;">
+                  <a href="#!" type="submit" class="primary-btn" >Login</a>
                 </div>
               </form>
               
