383db894863e28067433febaaf5a7cd0783da3a4
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 23:44:08 +0600

    Commit Mon 08/12/2024 23:44:08.32 15064

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -32,7 +32,7 @@
                   </div>
                   <div class="col-lg-3">
                       <div class="categories__item set-bg" data-setbg="{{ asset('vendor/ogani/img/categories/cat-4.jpg') }}">
-                          <h5><a href="{{ Request::url() . '/category/10/canned-dried' }}">Fruit Drinks</a></h5>
+                          <h5><a href="{{ Request::url() . '/category/10/canned-dried' }}">Dal (Lentils/Pulses)</a></h5>
                       </div>
                   </div>
                   <div class="col-lg-3">
