1ac93dea9de6a9363dab3a6a61625c1209a82ff1
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 23:30:46 +0600

    Commit Mon 08/12/2024 23:30:46.23 30184

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -14,7 +14,7 @@
               <div class="categories__slider owl-carousel">
                   <div class="col-lg-3">
                       <div class="categories__item set-bg" data-setbg="{{ asset('vendor/ogani/img/categories/cat-1.jpg') }}">
-                          <h5><a href="#">Dried</a></h5>
+                          <h5><a href="#">Dried & Canned</a></h5>
                       </div>
                   </div>
                   <div class="col-lg-3">
