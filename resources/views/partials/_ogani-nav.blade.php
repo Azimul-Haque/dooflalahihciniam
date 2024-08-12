fe28495e17f1c8d6f901847a24a9dad5daca66a0
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 22:52:53 +0600

    Commit Mon 08/12/2024 22:52:53.22 26448

--- resources/views/partials/_ogani-nav.blade.php
+++ resources/views/partials/_ogani-nav.blade.php
@@ -330,7 +330,7 @@
                               <div class="hero__item set-bg" data-setbg="{{ asset('vendor/ogani/img/hero/banner.jpg') }}">
                                   <div class="hero__text">
                                       <span>ALWAYS FRESH</span>
-                                      <h2>Vegetable <br />100% Organic</h2>
+                                      <h2>Beef <br />100% Halal</h2>
                                       <p>Fresh & Halal Food | Your right Choice</p>
                                       <a href="{{ route('product.shop') }}" class="success-btn">SHOP NOW</a>
                                   </div>
