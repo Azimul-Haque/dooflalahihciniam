711c1f75fbe1edc9616911902ae351ab6463e768
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 22:29:09 +0600

    Commit Mon 08/12/2024 22:29:09.28 4242

--- resources/views/auth/emails/password.blade.php
+++ resources/views/auth/emails/password.blade.php
@@ -15,7 +15,7 @@
 	      </p>
 	      <br/><br/>
 	      <p style="font-size: 12px; color: #ACACAC;">
-	        This is a auto-generated email from Loyal অভিযাত্রী. This email arrived to you because you (or may be someone else!) have requested to reset the password associated with this email address. If you are getting this email by mistake, please ignore it.
+	        This is a auto-generated email from <b>Mainichi</b> Halal Food. This email arrived to you because you (or may be someone else!) have requested to reset the password associated with this email address. If you are getting this email by mistake, please ignore it.
 	      </p>
 	      <p style="font-size: 12px; color: #ACACAC;">
 	        &copy; @php echo date('Y'); @endphp <a href="http://loyalovijatri.com/"><b>Mainichi</b> Halal Food</a>, Japan
