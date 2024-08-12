ea0d2210fc0ef1a4d59c90c3dcf11421660b9d78
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-13 00:45:25 +0600

    Commit Tue 08/13/2024  0:45:25.27 24952

--- resources/views/warehouse/produtcs.blade.php
+++ resources/views/warehouse/produtcs.blade.php
@@ -142,7 +142,7 @@
                       </div>
 
                       @if($product->isAvailable == 1)
-                      <a type="button" class="btn btn-sm btn-warning" data-toggle="modal" data-target="#makeFeatured{{ $product->id }}" data-backdrop="static" title="Make this product unavailable"><i class="fa fa-tag" aria-hidden="true"></i></a>
+                      <a type="button" class="btn btn-sm btn-info" data-toggle="modal" data-target="#makeFeatured{{ $product->id }}" data-backdrop="static" title="Make this product unavailable"><i class="fa fa-tag" aria-hidden="true"></i></a>
                       @elseif($product->isAvailable == 0)
                       <a type="button" class="btn btn-sm btn-default" data-toggle="modal" data-target="#makeFeatured{{ $product->id }}" data-backdrop="static" title="Make this product available"><i class="fa fa-tag" aria-hidden="true"></i></a>
                       @endif
@@ -152,7 +152,7 @@
                         
                           <!-- Modal content-->
                           <div class="modal-content">
-                            <div class="modal-header modal-header-warning">
+                            <div class="modal-header modal-header-info">
                               <button type="button" class="close" data-dismiss="modal">×</button>
                               <h4 class="modal-title">আপনি কি নিশ্চিতভাবে এই পণ্যটিকে 
                               @if($product->isAvailable == 1)
