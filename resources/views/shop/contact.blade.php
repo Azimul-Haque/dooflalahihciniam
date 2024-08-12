cae442bde3787bca2a054e1169477b32ec4156e7
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-12 22:45:44 +0600

    Commit Mon 08/12/2024 22:45:44.23 14967

--- resources/views/shop/contact.blade.php
+++ resources/views/shop/contact.blade.php
@@ -123,47 +123,6 @@
         </div>
     </div>
     <!-- Contact Form End -->
-
-    <section class="wow fadeIn">
-        <div class="container">
-            <div class="row">
-                
-
-                <div class="col-md-6 col-sm-6">
-                    <span class="text-large letter-spacing-2 black-text font-weight-600 agency-title">Contact Form</span><br/><br/><br/>
-                    {!! Form::open(['route' => 'index.postcontactmessage', 'method' => 'POST']) !!}
-                        <div id="success" class="no-margin-lr"></div>
-                        <input name="name" type="text" value="{{ old('name') }}" placeholder="Name" required="" />
-                        <div class="row">
-                            <div class="col-md-6">
-                                <input name="phone" type="text" value="{{ old('phone') }}" placeholder="Phone Number"  required="" />
-                            </div>
-                            <div class="col-md-6">
-                                <input name="email" type="email" value="{{ old('email') }}" placeholder="Email"  required="" />
-                            </div>
-                        </div>
-                        <textarea name="message" placeholder="Message"  required="">{{ old('message') }}</textarea>
-                        
-                        
-                        <input type="text" name="contact_sum_result" id="" class="form-control" >
-                        {!! app('captcha')->display() !!}<br/>
-                        
-                        <button id="contact-us-button" type="submit" class="highlight-button-dark btn btn-small button xs-margin-bottom-five"><i class="fa fa-paper-plane"></i> Send</button>
-                    {!! Form::close() !!}
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <section class="wow fadeIn no-padding">
-        <div class="container-fuild">
-            <div class="row no-margin">
-                <div id="canvas1" class="col-md-12 col-sm-12 no-padding contact-map map">
-                    <iframe id="map_canvas1" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d810.0157399575658!2d140.09124118781082!3d35.70006825145883!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6022812f4f57067f%3A0x913d9961d26e44bf!2zSmFwYW4sIOOAkjI3Ni0wMDMyIENoaWJhLCBZYWNoaXlvLCBZYWNoaXlvZGFpaGlnYXNoaSwgMS1jaMWNbWXiiJIy4oiSMSAyLTE2IOesrDc!5e0!3m2!1sen!2sbd!4v1715632587410!5m2!1sen!2sbd"></iframe>
-                </div>
-            </div>
-        </div>
-    </section>
 @endsection
 
 @section('js')
