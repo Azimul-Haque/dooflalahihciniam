fd254d7fa9fb83f59a0cf18ff862e5f6dd71b067
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-14 03:01:50 +0600

    Commit Wed 08/14/2024  3:01:50.26 27763

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -84,7 +84,7 @@
                   </div>
               </div>
 
-              <script src="http://localhost:8000/vendor/ogani/js/jquery-3.3.1.min.js"></script>
+              <script src="{{ asset('vendor/ogani/js/jquery-3.3.1.min.js') }}"></script>
               <script type="text/javascript">
                 $(document).ready(function(){
                     $("#addToCart{{ $product->id }}").click(function(){
