27fdbce979157f60c71a7ab05892000ffe0cde58
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-14 02:52:25 +0600

    Commit Wed 08/14/2024  2:52:25.24 31558

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -70,7 +70,7 @@
           </div>
           <div class="row featured__filter">
             @foreach($featureditems as $featureditem)
-              <div class="col-lg-3 col-md-4 col-sm-6 mix @if($featureditem->category->name) @endif">
+              <div class="col-lg-3 col-md-4 col-sm-6 mix @if($featureditem->category->id == 1) beef @endif">
                   <div class="featured__item">
                       <div class="featured__item__pic set-bg" data-setbg="{{ asset('vendor/ogani/img/featured/feature-1.jpg') }}">
                           <ul class="featured__item__pic__hover">
