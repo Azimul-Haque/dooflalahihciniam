545e5950308a85afb6814ce53a070a9c2be3c819
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-14 02:49:41 +0600

    Commit Wed 08/14/2024  2:49:41.46 20215

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -69,6 +69,9 @@
               </div>
           </div>
           <div class="row featured__filter">
+            @foreach($featureditems as $featureditem)
+
+            @endforeach
               <div class="col-lg-3 col-md-4 col-sm-6 mix beef chicken">
                   <div class="featured__item">
                       <div class="featured__item__pic set-bg" data-setbg="{{ asset('vendor/ogani/img/featured/feature-1.jpg') }}">
