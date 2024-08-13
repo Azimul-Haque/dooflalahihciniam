ec17f1a34bc5631db49f4fe2c58b473776822015
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-14 03:39:56 +0600

    Commit Wed 08/14/2024  3:39:56.68 429

--- resources/views/shop/shop.blade.php
+++ resources/views/shop/shop.blade.php
@@ -14,7 +14,7 @@
         <div class="row">
             <div class="col-lg-12 text-center">
                 <div class="breadcrumb__text">
-                    <h2>Organi Shop</h2>
+                    <h2>Mainichi Halal Food Shop</h2>
                     <div class="breadcrumb__option">
                         <a href="{{ route('product.index') }}">Home</a>
                         <span>Shop</span>
