31260c8abb374e97ce809ea4e6a660e990ddc2d5
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-14 03:41:02 +0600

    Commit Wed 08/14/2024  3:41:02.26 15912

--- resources/views/partials/_ogani-nav.blade.php
+++ resources/views/partials/_ogani-nav.blade.php
@@ -290,7 +290,7 @@
                             </form>
                         </div>
                         <div class="hero__search__phone">
-                            <a class="hero__search__phone__icon" href="#!">
+                            <a class="hero__search__phone__icon" href="tel:+81 090-1703-9984">
                                 <i class="fa fa-phone"></i>
                             </a>
                             <div class="hero__search__phone__text">
