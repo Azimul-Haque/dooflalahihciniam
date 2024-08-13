e206d67bf71fa4f44cbcdab8c7855211567b69ce
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-14 03:09:00 +0600

    Commit Wed 08/14/2024  3:09:00.32 17341

--- resources/views/partials/_ogani-shop-left-menu.blade.php
+++ resources/views/partials/_ogani-shop-left-menu.blade.php
@@ -146,54 +146,6 @@
         </div>
     </div> --}}
     <div class="sidebar__item">
-        <div class="latest-product__text">
-            <h4>Latest Products</h4>
-            <div class="latest-product__slider owl-carousel">
-                <div class="latest-prdouct__slider__item">
-                    @foreach($newarrivals->slice(0, 3) as $product)
-                      @php
-                          $ptext = $product->title;
-                          $pslug = \Illuminate\Support\Str::slug(preg_replace('/[^\w\d]+/', '-', $ptext));
-                      @endphp
-                      <a href="{{ route('product.getsingleproduct', [$product->id, $pslug]) }}" class="latest-product__item">
-                          <div class="latest-product__item__pic">
-                              <img src="{{ asset('vendor/ogani/img/latest-product/lp-1.jpg') }}" alt="">
-                          </div>
-                          <div class="latest-product__item__text">
-                              <h6>{{ $product->title }}</h6>
-                              <span>
-                                  @if($product->oldprice > 0)
-                                    <small><del>¥ {{ $product->oldprice }}</del></small>
-                                  @endif
-                                  ¥ {{ $product->price }}
-                              </span>
-                          </div>
-                      </a>
-                    @endforeach
-                </div>
-                <div class="latest-prdouct__slider__item">
-                    @foreach($newarrivals->slice(3, 3) as $product)
-                      @php
-                          $ptext = $product->title;
-                          $pslug = \Illuminate\Support\Str::slug(preg_replace('/[^\w\d]+/', '-', $ptext));
-                      @endphp
-                      <a href="{{ route('product.getsingleproduct', [$product->id, $pslug]) }}" class="latest-product__item">
-                          <div class="latest-product__item__pic">
-                              <img src="{{ asset('vendor/ogani/img/latest-product/lp-1.jpg') }}" alt="">
-                          </div>
-                          <div class="latest-product__item__text">
-                              <h6>{{ $product->title }}</h6>
-                              <span>
-                                  @if($product->oldprice > 0)
-                                    <small><del>¥ {{ $product->oldprice }}</del></small>
-                                  @endif
-                                  ¥ {{ $product->price }}
-                              </span>
-                          </div>
-                      </a>
-                    @endforeach
-                </div>
-            </div>
-        </div>
+        
     </div>
 </div>
