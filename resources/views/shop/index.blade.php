1247b5b79d8dc84ee3e6744fc58fd41bf307e335
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-14 03:14:53 +0600

    Commit Wed 08/14/2024  3:14:53.29 23746

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -169,71 +169,10 @@
           <div class="row">
               <div class="col-lg-4 col-md-6">
                   @include('partials._ogani-latest-products')
+                  
               </div>
               <div class="col-lg-4 col-md-6">
-                  <div class="latest-product__text">
-                      <h4>Top Rated Products</h4>
-                      <div class="latest-product__slider owl-carousel">
-                          <div class="latest-prdouct__slider__item">
-                              <a href="#" class="latest-product__item">
-                                  <div class="latest-product__item__pic">
-                                      <img src="{{ asset('vendor/ogani/img/latest-product/lp-1.jpg') }}" alt="">
-                                  </div>
-                                  <div class="latest-product__item__text">
-                                      <h6>Crab Pool Security</h6>
-                                      <span>¥30.00</span>
-                                  </div>
-                              </a>
-                              <a href="#" class="latest-product__item">
-                                  <div class="latest-product__item__pic">
-                                      <img src="{{ asset('vendor/ogani/img/latest-product/lp-2.jpg') }}" alt="">
-                                  </div>
-                                  <div class="latest-product__item__text">
-                                      <h6>Crab Pool Security</h6>
-                                      <span>¥30.00</span>
-                                  </div>
-                              </a>
-                              <a href="#" class="latest-product__item">
-                                  <div class="latest-product__item__pic">
-                                      <img src="{{ asset('vendor/ogani/img/latest-product/lp-3.jpg') }}" alt="">
-                                  </div>
-                                  <div class="latest-product__item__text">
-                                      <h6>Crab Pool Security</h6>
-                                      <span>¥30.00</span>
-                                  </div>
-                              </a>
-                          </div>
-                          <div class="latest-prdouct__slider__item">
-                              <a href="#" class="latest-product__item">
-                                  <div class="latest-product__item__pic">
-                                      <img src="{{ asset('vendor/ogani/img/latest-product/lp-1.jpg') }}" alt="">
-                                  </div>
-                                  <div class="latest-product__item__text">
-                                      <h6>Crab Pool Security</h6>
-                                      <span>¥30.00</span>
-                                  </div>
-                              </a>
-                              <a href="#" class="latest-product__item">
-                                  <div class="latest-product__item__pic">
-                                      <img src="{{ asset('vendor/ogani/img/latest-product/lp-2.jpg') }}" alt="">
-                                  </div>
-                                  <div class="latest-product__item__text">
-                                      <h6>Crab Pool Security</h6>
-                                      <span>¥30.00</span>
-                                  </div>
-                              </a>
-                              <a href="#" class="latest-product__item">
-                                  <div class="latest-product__item__pic">
-                                      <img src="{{ asset('vendor/ogani/img/latest-product/lp-3.jpg') }}" alt="">
-                                  </div>
-                                  <div class="latest-product__item__text">
-                                      <h6>Crab Pool Security</h6>
-                                      <span>¥30.00</span>
-                                  </div>
-                              </a>
-                          </div>
-                      </div>
-                  </div>
+                  @include('partials._ogani-top-rated')
               </div>
               <div class="col-lg-4 col-md-6">
                   <div class="latest-product__text">
