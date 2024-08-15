82672957018892b80b7aaad609243d33b0dd044a
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-16 02:57:21 +0600

    Commit Fri 08/16/2024  2:57:21.28 7545

--- resources/views/shop/shoppingcart.blade.php
+++ resources/views/shop/shoppingcart.blade.php
@@ -190,8 +190,8 @@
                   <div class="shoping__checkout">
                       <h5>Cart Total</h5>
                       <ul>
-                          <li>Subtotal <span>¥ <span>{{ $totalPrice }}</span></span></li>
-                          <li>Total <span>¥ {{ $totalPrice }}</span></li>
+                          <li>Subtotal <span>¥ <span id="totalPriceGross">{{ $totalPrice }}</span></span></li>
+                          <li>Total <span>¥ <span id="totalPriceGross">{{ $totalPrice }}</span></span></li>
                       </ul>
                       <a href="{{ route('product.checkout') }}" class="primary-btn">PROCEED TO CHECKOUT</a>
                   </div>
