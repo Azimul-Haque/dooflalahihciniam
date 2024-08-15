600c4aa2f2c693245ef4b144786458621df46b92
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-16 04:34:09 +0600

    Commit Fri 08/16/2024  4:34:09.32 8698

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -157,14 +157,14 @@
               <div class="card card-success shadow-light">
                 <div class="card-header">
                   <h4 class="card-title">
-                    <button class="" data-toggle="collapse" data-parent="#accordion" data-target="#receiptcollapse{{ $order->id }}">
+                    <a data-toggle="collapse" data-parent="#accordion" data-target="#receiptcollapse{{ $order->id }}">
                       @if($order->paymentstatus == 'paid')
                         <i class="fa fa-check" title="Delivered"></i>
                       @elseif($order->paymentstatus == 'not-paid')
                         <i class="fa fa-hourglass-start" title="Yet to deliver"></i>
                       @endif
                       {{ $order->created_at->format('M d, Y, h:i A') }}
-                    </button>
+                    </a>
                   </h4>
                 </div>
                 <div id="receiptcollapse{{ $order->id }}" class="card-collapse collapse">
