c4a1a88232eee9198f4e1d03cabcba6e180f826b
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-16 02:36:12 +0600

    Commit Fri 08/16/2024  2:36:12.33 10080

--- resources/views/shop/shoppingcart.blade.php
+++ resources/views/shop/shoppingcart.blade.php
@@ -118,7 +118,7 @@
                       </table>
                   </div>
                 @else
-                  <div class="col-md-10 col-auto">
+                  <div class="col-md-10 col-md-auto">
                     <h2>
                       <center>
                         There are no products in your bag! Visit this page by adding our products to your bag. thank you<br/><br/>
