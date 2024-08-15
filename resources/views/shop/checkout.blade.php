ba06b60b82c13d41904efe5f526f6c5d94054812
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-16 03:20:34 +0600

    Commit Fri 08/16/2024  3:20:34.27 12975

--- resources/views/shop/checkout.blade.php
+++ resources/views/shop/checkout.blade.php
@@ -67,7 +67,7 @@
                           </li>
                           @endforeach
                         </ul>
-                        <div class="checkout__order__subtotal">Delivery Charge <span id="deliveryCharge"><span>¥</span>{{ $cart->deliveryCharge }}</span></div>
+                        <div class="checkout__order__subtotal">Delivery Charge <span>¥</span><span id="deliveryCharge">{{ $cart->deliveryCharge }}</span></div>
                         <div class="checkout__order__total">Subtotal <span>$750.99</span></div>
                         <div class="checkout__order__total">Total <span>$750.99</span></div>
                         <div class="checkout__input__checkbox">
