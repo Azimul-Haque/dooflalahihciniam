a327b3fb45faf00a0bf7cbf6afd0606985fc7110
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-16 04:00:49 +0600

    Commit Fri 08/16/2024  4:00:49.30 12104

--- resources/views/partials/_profile.blade.php
+++ resources/views/partials/_profile.blade.php
@@ -14,7 +14,7 @@
 				<div class="profile-usertitle-name">
 					<big><b>{{ Auth::user()->name }}</b></big>
 				</div>
-				<div class="">
+				<div class="profile-usertitle-job">
 					<span class="badge badge-pill badge-default">{{ ucfirst(Auth::user()->role) }}</span><br/><hr/>
 				</div>
 			</center>
