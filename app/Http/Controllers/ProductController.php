fc45bbb20aae13c802717f98b9e464d1b7864dbd
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-16 05:02:44 +0600

    Commit Fri 08/16/2024  5:02:44.23 1535

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -361,8 +361,7 @@ class ProductController extends Controller
         // apatoto payment_method off 
         // apatoto payment_method off 
         // $order->payment_method = $request->payment_method; // 0 means cash on delivery, 1 means bKash
-        // $order->deliverylocation = $request->deliverylocation; // 0 == Dhaka, 1020 = free pickup, 2 = outside of Dhaka
-        $order->deliverylocation = 0;
+        $order->deliverylocation = $request->deliverylocation; // 0 == Address, 1020 = Shop
         $nowdatetime = Carbon::now();
 
         // $order->payment_id = $nowdatetime->format('YmdHis') . random_string(5);
