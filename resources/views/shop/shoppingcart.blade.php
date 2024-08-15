a4db1b0672c7c799c54e490f399aaa58cdf943e1
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-16 02:24:59 +0600

    Commit Fri 08/16/2024  2:24:59.24 21896

--- resources/views/shop/shoppingcart.blade.php
+++ resources/views/shop/shoppingcart.blade.php
@@ -21,7 +21,7 @@
                       <h2>Shopping Cart</h2>
                       <div class="breadcrumb__option">
                           <a href="{{ route('product.index') }}">Home</a>
-                          <span>Shop</span>
+                          <span>Shopping Cart</span>
                       </div>
                   </div>
               </div>
