a3726202ee221fca80f00605769b9ba576f7b8ef
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-16 04:46:29 +0600

    Commit Fri 08/16/2024  4:46:29.28 26841

--- resources/views/shop/terms.blade.php
+++ resources/views/shop/terms.blade.php
@@ -32,6 +32,23 @@
           </div>
       </div>
   </section>
+  <!-- Breadcrumb Section Begin -->
+  <section class="breadcrumb-section set-bg" data-setbg="{{ asset('vendor/ogani/img/breadcrumb.jpg') }}">
+      <div class="container">
+          <div class="row">
+              <div class="col-lg-12 text-center">
+                  <div class="breadcrumb__text">
+                      {{-- <h2>Shopping Cart</h2> --}}
+                      <div class="breadcrumb__option">
+                          <a href="{{ route('product.index') }}">Home</a>
+                          <span>About Us</span>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </section>
+  <!-- Breadcrumb Section End -->
 
   <!-- content section -->
   <section class="padding-three">
