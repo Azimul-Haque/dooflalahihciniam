a5d7ccdb95db1d9e24790a2de0fd423e63396bcd
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-16 03:02:36 +0600

    Commit Fri 08/16/2024  3:02:36.27 11836

--- resources/views/shop/checkout.blade.php
+++ resources/views/shop/checkout.blade.php
@@ -12,6 +12,23 @@
 @endsection
 
 @section('content')
+  <!-- Breadcrumb Section Begin -->
+  <section class="breadcrumb-section set-bg" data-setbg="{{ asset('vendor/ogani/img/breadcrumb.jpg') }}">
+      <div class="container">
+          <div class="row">
+              <div class="col-lg-12 text-center">
+                  <div class="breadcrumb__text">
+                      {{-- <h2>Shopping Cart</h2> --}}
+                      <div class="breadcrumb__option">
+                          <a href="{{ route('product.index') }}">Home</a>
+                          <span>Shopping Cart</span>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </section>
+  <!-- Breadcrumb Section End -->
   <!-- head section -->
   <section class="content-top-margin page-title page-title-small bg-gray">
       <div class="container">
