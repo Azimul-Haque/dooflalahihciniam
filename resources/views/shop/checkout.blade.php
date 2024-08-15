b37330a819d35add21d133e23cc83cf349a5a15f
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-16 04:54:23 +0600

    Commit Fri 08/16/2024  4:54:23.25 20789

--- resources/views/shop/checkout.blade.php
+++ resources/views/shop/checkout.blade.php
@@ -77,7 +77,7 @@
                         <div class="checkout__order__subtotal">Subtotal <span>¥<span>{{ $cart->totalPrice }}</span></span></div>
                         {{-- <div class="checkout__order__total">Delivery Charge <span>¥<span id="deliveryCharge">{{ $cart->deliveryCharge }}</span></span></div> --}}
                         <div class="checkout__order__total">Delivery Charge <span>¥<span id="deliveryCharge">60</span></span></div>
-                        <div class="checkout__order__total">Delivery Charge <span>¥<span id="totalPrice">{{ $cart->totalPrice }}</span></span></div>
+                        <div class="checkout__order__total">Delivery Charge <span>¥<span id="totalPrice">{{ $cart->totalPrice + 60 }}</span></span></div>
                         
                         <button type="submit" class="site-btn">PLACE ORDER</button>
 
