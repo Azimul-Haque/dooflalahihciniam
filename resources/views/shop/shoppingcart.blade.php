faaf389d43cdffed2a47de7a357aadf217b776f3
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-16 02:28:39 +0600

    Commit Fri 08/16/2024  2:28:39.35 554

--- resources/views/shop/shoppingcart.blade.php
+++ resources/views/shop/shoppingcart.blade.php
@@ -18,7 +18,7 @@
           <div class="row">
               <div class="col-lg-12 text-center">
                   <div class="breadcrumb__text">
-                      <h2>Shopping Cart</h2>
+                      <h4>Shopping Cart</h4>
                       <div class="breadcrumb__option">
                           <a href="{{ route('product.index') }}">Home</a>
                           <span>Shopping Cart</span>
@@ -29,7 +29,7 @@
       </div>
   </section>
   <!-- Breadcrumb Section End -->
-  
+
   
   <!-- content section -->
   <section class="content-section padding-three">
