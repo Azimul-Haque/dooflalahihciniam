d76087e3c8ef301e43591c629ddbb3a120c34e66
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-16 04:23:13 +0600

    Commit Fri 08/16/2024  4:23:13.32 12053

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -140,18 +140,7 @@
             </center>
             @endif
             <br/>
-            <div class="card card-primary shadow-light">
-              <div class="card-heading">
-                <h4 class="card-title">Your previous orders</h4>
-              </div>
-              <div class="card-body">
-                <div class="card-group" id="accordion">
-                
-                <br/>
-                @include('pagination.default', ['paginator' => $orders])
-                </div>
-              </div>
-            </div>
+            
             @foreach($orders as $order)
               <div class="card card-success shadow-light">
                 <div class="card-header">
