bfe4513739c9c8d7a9b43a5353b0c5d45a8b6e1c
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-16 04:45:24 +0600

    Commit Fri 08/16/2024  4:45:24.31 21802

--- resources/views/shop/about.blade.php
+++ resources/views/shop/about.blade.php
@@ -25,7 +25,7 @@
                       {{-- <h2>Shopping Cart</h2> --}}
                       <div class="breadcrumb__option">
                           <a href="{{ route('product.index') }}">Home</a>
-                          <span>{{ Auth::user()->name }}</span>
+                          <span>About Us</span>
                       </div>
                   </div>
               </div>
