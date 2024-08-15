7ce02445e9481088849ea0e92d017b4f6af050c8
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-16 02:55:22 +0600

    Commit Fri 08/16/2024  2:55:22.28 17557

--- resources/views/shop/shoppingcart.blade.php
+++ resources/views/shop/shoppingcart.blade.php
@@ -56,7 +56,7 @@
                             <div class="row">
                               <div class="col-lg-6 col-md-5 col-sm-5 col-xs-12">
                                 <center>
-                                  <div class="btn-group" style="min-width: 100px;">
+                                  <div class="btn-group" style="max-width: 100px;">
                                     <button id="reducebyone{{ $product['item']['id'] }}" class="btn primary-btn btn-sm" data-toggle="tooltip" data-placement="bottom" title="Reduce Item"><i class="fa fa-minus white-text" aria-hidden="true"></i></button>
                                     <a id="itemQtyInBag{{ $product['item']['id'] }}" class="btn primary-btn btn-sm disabled"><span>{{ $product['qty'] }}</span></a>
                                     <button id="addbyone{{ $product['item']['id'] }}" class="btn primary-btn btn-sm" data-toggle="tooltip" data-placement="bottom" title="Increase Item"><i class="fa fa-plus white-text" aria-hidden="true"></i></button>
