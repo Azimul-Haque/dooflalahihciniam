5c4a7cc1815316a790b4a2f7a9bf831554982286
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-16 04:09:57 +0600

    Commit Fri 08/16/2024  4:09:57.29 12208

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -83,44 +83,22 @@
                 <h4>Top Rated Products</h4>
                 <div class="latest-product__slider owl-carousel">
                     <div class="latest-prdouct__slider__item">
-                        @foreach($topratedproducts->slice(0, 3) as $product)
+                        @foreach($wishlists as $wishlist)
                           @php
-                              $ptext = $product->title;
+                              $ptext = $wishlist->title;
                               $pslug = \Illuminate\Support\Str::slug(preg_replace('/[^\w\d]+/', '-', $ptext));
                           @endphp
-                          <a href="{{ route('product.getsingleproduct', [$product->id, $pslug]) }}" class="latest-product__item">
+                          <a href="{{ route('product.getsingleproduct', [$wishlist->id, $pslug]) }}" class="latest-product__item">
                               <div class="latest-product__item__pic">
-                                  <img src="{{ asset('images/product-images/'.$product->productimages->first()->image) }}" alt="{{ 'Image of Product' }}">
+                                  <img src="{{ asset('images/product-images/'.$wishlist->productimages->first()->image) }}" alt="{{ 'Image of Product' }}">
                               </div>
                               <div class="latest-product__item__text">
-                                  <h6>{{ $product->title }}</h6>
+                                  <h6>{{ $wishlist->title }}</h6>
                                   <span>
-                                      @if($product->oldprice > 0)
-                                        <small><del>¥ {{ $product->oldprice }}</del></small>
+                                      @if($wishlist->oldprice > 0)
+                                        <small><del>¥ {{ $wishlist->oldprice }}</del></small>
                                       @endif
-                                      ¥ {{ $product->price }}
-                                  </span>
-                              </div>
-                          </a>
-                        @endforeach
-                    </div>
-                    <div class="latest-prdouct__slider__item">
-                        @foreach($topratedproducts->slice(3, 3) as $product)
-                          @php
-                              $ptext = $product->title;
-                              $pslug = \Illuminate\Support\Str::slug(preg_replace('/[^\w\d]+/', '-', $ptext));
-                          @endphp
-                          <a href="{{ route('product.getsingleproduct', [$product->id, $pslug]) }}" class="latest-product__item">
-                              <div class="latest-product__item__pic">
-                                  <img src="{{ asset('images/product-images/'.$product->productimages->first()->image) }}" alt="{{ 'Image of Product' }}">
-                              </div>
-                              <div class="latest-product__item__text">
-                                  <h6>{{ $product->title }}</h6>
-                                  <span>
-                                      @if($product->oldprice > 0)
-                                        <small><del>¥ {{ $product->oldprice }}</del></small>
-                                      @endif
-                                      ¥ {{ $product->price }}
+                                      ¥ {{ $wishlist->price }}
                                   </span>
                               </div>
                           </a>
