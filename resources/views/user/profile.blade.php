1f45d5007ebaa5ca2955765052a666ba274f63fe
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-16 05:00:28 +0600

    Commit Fri 08/16/2024  5:00:28.33 1183

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -180,7 +180,7 @@
                         @if($order->deliverylocation == 1020)
                           {{ deliverylocation($order->deliverylocation) }}
                         @else
-                          {{ $order->deliverylocation }}
+                          {{ deliverylocation($order->deliverylocation) }}
                         @endif
                       </span>
                       <ul class="list-group" style="margin-bottom: 5px;">
