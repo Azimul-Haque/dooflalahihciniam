6fadbedefd6bbddde4acac6169bb4b168566b788
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-16 02:50:50 +0600

    Commit Fri 08/16/2024  2:50:50.29 316

--- resources/views/shop/shoppingcart.blade.php
+++ resources/views/shop/shoppingcart.blade.php
@@ -56,11 +56,13 @@
                         <div class="col-md-6 col-sm-6 col-xs-6">
                           <div class="row">
                             <div class="col-lg-6 col-md-5 col-sm-5 col-xs-12">
-                              <div class="btn-group">
-                                <button id="reducebyone{{ $product['item']['id'] }}" class="btn btn-primary btn-sm" data-toggle="tooltip" data-placement="bottom" title="Reduce Item"><i class="fa fa-minus white-text" aria-hidden="true"></i></button>
-                                <a id="itemQtyInBag{{ $product['item']['id'] }}" class="btn btn-primary btn-sm disabled"><span>{{ $product['qty'] }}</span></a>
-                                <button id="addbyone{{ $product['item']['id'] }}" class="btn btn-primary btn-sm" data-toggle="tooltip" data-placement="bottom" title="Increase Item"><i class="fa fa-plus white-text" aria-hidden="true"></i></button>
-                              </div>
+                              <center>
+                                <div class="btn-group">
+                                  <button id="reducebyone{{ $product['item']['id'] }}" class="btn btn-primary btn-sm" data-toggle="tooltip" data-placement="bottom" title="Reduce Item"><i class="fa fa-minus white-text" aria-hidden="true"></i></button>
+                                  <a id="itemQtyInBag{{ $product['item']['id'] }}" class="btn btn-primary btn-sm disabled"><span>{{ $product['qty'] }}</span></a>
+                                  <button id="addbyone{{ $product['item']['id'] }}" class="btn btn-primary btn-sm" data-toggle="tooltip" data-placement="bottom" title="Increase Item"><i class="fa fa-plus white-text" aria-hidden="true"></i></button>
+                                </div>
+                              </center>
                             </div>
                             <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 item-center">
                               <span id="itemTotalPrice{{ $product['item']['id'] }}" class="">Total: ¥ {{ $product['price'] }}</span>
