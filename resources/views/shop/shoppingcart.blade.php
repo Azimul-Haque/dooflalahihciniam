8ddc619407794ccddc0a72e3b63f23589376493e
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-16 02:38:28 +0600

    Commit Fri 08/16/2024  2:38:28.25 28524

--- resources/views/shop/shoppingcart.blade.php
+++ resources/views/shop/shoppingcart.blade.php
@@ -117,17 +117,6 @@
                           </tbody>
                       </table>
                   </div>
-                @else
-                  <div class="col-md-10 mx-auto">
-                    <h2>
-                      <center>
-                        There are no products in your bag! Visit this page by adding our products to your bag. thank you<br/><br/>
-                        <a href="{{ route('product.index') }}" class="highlight-button btn btn-medium"><i class="fa fa-cart-plus"></i> See Products</a>
-                      </center>
-                    </h2>
-
-                  </div>
-                @endif
               </div>
           </div>
           <div class="row">
