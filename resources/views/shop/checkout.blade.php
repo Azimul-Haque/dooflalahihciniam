2bf4643cdee93dc62a5f67da704ab64c10a489cc
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-16 03:06:04 +0600

    Commit Fri 08/16/2024  3:06:04.27 17620

--- resources/views/shop/checkout.blade.php
+++ resources/views/shop/checkout.blade.php
@@ -41,6 +41,15 @@
           </div> --}}
           <div class="checkout__form">
               <h4>Billing Details</h4>
+              <div class="col-md-6">
+                <h2>Customer: {{ Auth::user()->name }}</h2>
+                ID: {{ Auth::user()->code }}<br/>
+                Contact: {{ Auth::user()->phone }}<br/>
+                Email: {{ Auth::user()->email }}<br/>
+                <big>Earned points: <b>{{ Auth::user()->points }}</b></big>
+                <br/><br/>
+              </div>
+
               <div class="col-md-4">
                 <h2>Customer: {{ Auth::user()->name }}</h2>
                 ID: {{ Auth::user()->code }}<br/>
