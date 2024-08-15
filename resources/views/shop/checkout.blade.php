d3961b79b9d0015948ee38a606cee5d4132ba161
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-16 03:10:00 +0600

    Commit Fri 08/16/2024  3:10:00.23 12668

--- resources/views/shop/checkout.blade.php
+++ resources/views/shop/checkout.blade.php
@@ -51,12 +51,7 @@
                 <br/><br/>
               </div>
               <div class="col-md-6">
-                @foreach($cart->items as $item)
-                <li class="list-group-item">
-                  {{-- {{ json_encode($item) }} --}}
-                  {{ $item['item']['title'] }} | {{ $item['qty'] }}
-                  <span class="right">৳ {{ $item['price'] }}</span>
-                </li>
+                
                 @endforeach
                   <div class="checkout__order">
                       <h4>Your Order</h4>
