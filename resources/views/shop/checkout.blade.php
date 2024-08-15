2568bc6a1175bc33194c4c01ddf31d36bb6c9fbe
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-16 03:13:45 +0600

    Commit Fri 08/16/2024  3:13:45.28 2671

--- resources/views/shop/checkout.blade.php
+++ resources/views/shop/checkout.blade.php
@@ -129,7 +129,7 @@
                         <div class="row">
                           <div class="col-md-4">
                             <label for="deliverylocation">Delivery Location</label>
-                            <select id="deliverylocation" name="deliverylocation" class="form-control" required="" onchange="changeDeliveryLocation()">
+                            <select id="deliverylocation" name="deliverylocation" class="" required="" onchange="changeDeliveryLocation()">
                               <option value="" selected="" disabled="">Select Location</option>
                               <option value="0">Inside Dhaka</option>
                               <option value="1020">Free Pick-up Point</option>
