dbee2bad73e6e7ee5999bed53268e7df3355c47e
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-16 02:25:18 +0600

    Commit Fri 08/16/2024  2:25:18.29 10486

--- resources/views/shop/shoppingcart.blade.php
+++ resources/views/shop/shoppingcart.blade.php
@@ -30,27 +30,7 @@
   </section>
   <!-- Breadcrumb Section End -->
   
-  <!-- head section -->
-  <section class="content-top-margin page-title page-title-small bg-gray">
-      <div class="container">
-          <div class="row">
-              <div class="col-lg-8 col-md-7 col-sm-12 wow fadeInUp" data-wow-duration="300ms">
-                  <!-- page title -->
-                  <h1 class="black-text">Your Orders</h1>
-                  <!-- end page title -->
-              </div>
-              <div class="col-lg-4 col-md-5 col-sm-12 breadcrumb text-uppercase wow fadeInUp xs-display-none" data-wow-duration="600ms">
-                  <!-- breadcrumb -->
-                  <ul>
-                      <li><a href="#">Home</a></li>
-                      <li>Shopping Cart</li>
-                  </ul>
-                  <!-- end breadcrumb -->
-              </div>
-          </div>
-      </div>
-  </section>
-  <!-- end head section -->
+  
   <!-- content section -->
   <section class="content-section padding-three">
       <div class="container">
