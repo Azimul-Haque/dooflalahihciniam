324c06860c7b9e81533a8c4ca93593db3a09a232
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-16 03:18:12 +0600

    Commit Fri 08/16/2024  3:18:12.28 6856

--- resources/views/shop/checkout.blade.php
+++ resources/views/shop/checkout.blade.php
@@ -44,7 +44,7 @@
             {!! Form::open(['route' => 'product.checkout', 'method' => 'POST']) !!}
               <div class="row">
                 <div class="col-md-6">
-                  <h3>Customer: {{ Auth::user()->name }}</h3>
+                  <big>Customer: {{ Auth::user()->name }}</big>
                   ID: {{ Auth::user()->code }}<br/>
                   Contact: {{ Auth::user()->phone }}<br/>
                   Email: {{ Auth::user()->email }}<br/>
