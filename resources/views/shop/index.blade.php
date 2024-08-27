9fa7c235c9206861f14f43e316abe69d7fad9d3b
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-28 01:08:55 +0600

    Commit Wed 08/28/2024  1:08:55.24 22749

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -143,9 +143,9 @@
                           success: function (data) {
                             var response = data;
                             console.log(response);
-                            $("#navTotalPrice").text('¥' +response);
-                            $("#navTotalPriceMobile").text('¥' +response);
-                            $("#navTotalPriceScroll").text('¥' +response);
+                            $("#navTotalPrice").text('¥ ' +response);
+                            $("#navTotalPriceMobile").text('¥ ' +response);
+                            $("#navTotalPriceScroll").text('¥ ' +response);
                           }
                       });
                     });
