eb4505df25ff71d345386cb0aa7ca8eb3a80087a
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-28 00:56:11 +0600

    Commit Wed 08/28/2024  0:56:11.29 10095

--- resources/views/shop/shoppingcart.blade.php
+++ resources/views/shop/shoppingcart.blade.php
@@ -113,6 +113,20 @@
                                   }
                               });
 
+                                $.ajax({
+                                    url: "/getcartdata",
+                                    type: "GET",
+                                    data: {},
+                                    success: function (data) {
+                                      var response = data;
+                                      console.log(response);
+                                      $("#navTotalPrice").text('¥' +response);
+                                      $("#navTotalPriceMobile").text('¥' +response);
+                                      $("#navTotalPriceScroll").text('¥' +response);
+                                    }
+                                });
+                              });
+
 
                             });
                             $("#addbyone{{ $product['item']['id'] }}").click(function(){
