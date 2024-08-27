384fc8817ba983615520bd494731c5afe58031ca
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-28 01:09:37 +0600

    Commit Wed 08/28/2024  1:09:37.32 18310

--- resources/views/shop/shoppingcart.blade.php
+++ resources/views/shop/shoppingcart.blade.php
@@ -121,9 +121,9 @@
                                   success: function (data) {
                                     var response = data;
                                     console.log(response);
-                                    $("#navTotalPrice").text('¥' +response);
-                                    $("#navTotalPriceMobile").text('¥' +response);
-                                    $("#navTotalPriceScroll").text('¥' +response);
+                                    $("#navTotalPrice").text('¥ ' +response);
+                                    $("#navTotalPriceMobile").text('¥ ' +response);
+                                    $("#navTotalPriceScroll").text('¥ ' +response);
                                   }
                               });
                             });
@@ -171,9 +171,9 @@
                                   success: function (data) {
                                     var response = data;
                                     console.log(response);
-                                    $("#navTotalPrice").text('¥' +response);
-                                    $("#navTotalPriceMobile").text('¥' +response);
-                                    $("#navTotalPriceScroll").text('¥' +response);
+                                    $("#navTotalPrice").text('¥ ' +response);
+                                    $("#navTotalPriceMobile").text('¥ ' +response);
+                                    $("#navTotalPriceScroll").text('¥ ' +response);
                                   }
                               });
                             });
