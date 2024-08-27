41d6b138bf555effc0240ea778f631fbb3ef7077
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-28 01:26:18 +0600

    Commit Wed 08/28/2024  1:26:18.41 686

--- resources/views/shop/categorywise.blade.php
+++ resources/views/shop/categorywise.blade.php
@@ -183,7 +183,7 @@
                                       <ul class="product__item__pic__hover">
                                           {{-- <li><a href="#"><i class="fa fa-heart"></i></a></li>
                                           <li><a href="#"><i class="fa fa-retweet"></i></a></li> --}}
-                                          <li><a id="addToCart{{ $product->id }}" href="#!"><i class="fa fa-shopping-cart"></i></a></li>
+                                          <li><a id="addToCart{{ $product->id }}" href="javascript:void(0)"><i class="fa fa-shopping-cart"></i></a></li>
                                       </ul>
                                   </div>
                                   <div class="product__item__text">
