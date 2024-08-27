d80af538b6a879c66465c61c59e2987723cf85c8
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-28 00:13:57 +0600

    Commit Wed 08/28/2024  0:13:57.34 8203

--- resources/views/partials/_ogani-nav.blade.php
+++ resources/views/partials/_ogani-nav.blade.php
@@ -23,9 +23,9 @@
                     <a href="{{ route('user.profile', Auth::user()->unique_key) }}#my_wishlist"><i class="fa fa-heart"></i> 
                         <span>
                             @if(Auth::user())
-
+                                {{ Auth::user()->wishlists->count() }}
                             @else
-                            
+
                             @endif
                         </span>
                     </a>
