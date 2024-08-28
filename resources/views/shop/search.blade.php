13723372083a9373110c2e3f9476171ebb47ceb3
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-29 03:00:39 +0600

    Commit Thu 08/29/2024  3:00:39.27 27899

--- resources/views/shop/search.blade.php
+++ resources/views/shop/search.blade.php
@@ -164,7 +164,7 @@
                             </div>
                             <div class="col-lg-4 col-md-4">
                                 <div class="filter__found">
-                                    <h6><span>{{ $products->count() }}</span> Products found</h6>
+                                    <h6><span>{{ $products->count() }}</span> Products found with the keyword ""</h6>
                                 </div>
                             </div>
                             <div class="col-lg-4 col-md-3">
