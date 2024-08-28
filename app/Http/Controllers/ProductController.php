df470bf0501b9940346472bfb4cf2a12fdbd9bc9
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-29 02:46:56 +0600

    Commit Thu 08/29/2024  2:46:56.29 26159

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -86,7 +86,7 @@ class ProductController extends Controller
                   
       $latestproducts = Product::orderBy('id', 'desc')->where('isAvailable', 1)->get()->take(6);
 
-      return view('shop.shop')
+      return view('shop.search')
                   ->withProducts($products)
                   ->withLatestproducts($latestproducts)
                   ->withSearchparam($search_param);
