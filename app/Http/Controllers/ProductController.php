9040ce1d865c443f9c59e3f60d7d72fa579f359b
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-29 03:44:13 +0600

    Commit Thu 08/29/2024  3:44:13.32 8115

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -85,12 +85,7 @@ class ProductController extends Controller
       //                    ->orderBy('id', 'desc')
       //                    ->paginate(10);
 
-      $products = Product::where('title', 'like', '%' . $search_param . '%')
-                        ->orWhere("code", 'LIKE', '%' . $search_param . '%')
-                        ->orWhere("shorttext", 'LIKE', '%' . $search_param . '%')
-                        ->orWhere("price", 'LIKE', '%' . $search_param . '%')
-                        ->orWhere("oldprice", 'LIKE', '%' . $search_param . '%')
-                        ->whereHas('category', function ($q) use ($search_param) {
+      $products = Product::whereHas('category', function ($q) use ($search_param) {
                             $q->where('name', 'like', '%' . $search_param . '%');
                         })
                         ->whereHas('subcategory', function ($q) use ($search_param) {
