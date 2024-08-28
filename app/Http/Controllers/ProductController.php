56c3c09d06b4eda08f033d356839f7cba1beb552
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-29 03:58:49 +0600

    Commit Thu 08/29/2024  3:58:49.33 28878

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -91,18 +91,12 @@ class ProductController extends Controller
                         ->orWhere("shorttext", 'LIKE', '%' . $search_param . '%')
                         ->orWhere("price", 'LIKE', '%' . $search_param . '%')
                         ->orWhere("oldprice", 'LIKE', '%' . $search_param . '%')
-                        ->whereHas('category' => function ($query) use ($search_param) {
-                             $query->where('name', 'like', '%' . $search_param . '%');
-                         })
-                        ->whereHas('subcategory' => function ($query) use ($search_param) {
-                             $query->where('name', 'like', '%' . $search_param . '%');
-                         })  
-                        // ->whereHas('category', function ($q) use ($search_param) {
-                        //     $q->where('name', 'like', '%' . $search_param . '%');
-                        // })
-                        // ->whereHas('subcategory', function ($q) use ($search_param) {
-                        //     $q->where('name', 'like', '%' . $search_param . '%');
-                        // })
+                        ->whereHas('category', function ($q) use ($search_param) {
+                            $q->where('name', 'like', '%' . $search_param . '%');
+                        })
+                        ->whereHas('subcategory', function ($q) use ($search_param) {
+                            $q->where('name', 'like', '%' . $search_param . '%');
+                        })
                         ->orderBy('id', 'desc')
                         ->paginate(10);
 
