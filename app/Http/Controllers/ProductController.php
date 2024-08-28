123524dfd4af0c288091187d4d3d08a1f3c6aaaa
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-29 03:49:47 +0600

    Commit Thu 08/29/2024  3:49:47.24 28207

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -77,6 +77,7 @@ class ProductController extends Controller
 
     public function searchProducts($search_param) 
     {
+      
       // $products = Product::where("title", 'LIKE', '%' . $search_param . '%')
       //                    ->orWhere("code", 'LIKE', '%' . $search_param . '%')
       //                    ->orWhere("shorttext", 'LIKE', '%' . $search_param . '%')
@@ -85,14 +86,7 @@ class ProductController extends Controller
       //                    ->orderBy('id', 'desc')
       //                    ->paginate(10);
 
-      $products = Product::where('title', 'like', '%' . $search_param . '%')
-                        ->orWhere("code", 'LIKE', '%' . $search_param . '%')
-                        ->orWhere("shorttext", 'LIKE', '%' . $search_param . '%')
-                        ->orWhere("price", 'LIKE', '%' . $search_param . '%')
-                        ->orWhere("oldprice", 'LIKE', '%' . $search_param . '%')
-                        ->whereHas('category', function ($q) use ($search_param) {
-                            $q->where('name', 'like', '%' . $search_param . '%');
-                        })
+      $products = Product::
                         ->whereHas('subcategory', function ($q) use ($search_param) {
                             $q->where('name', 'like', '%' . $search_param . '%');
                         })
