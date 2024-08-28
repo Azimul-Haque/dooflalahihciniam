e38458275e61413b34da12bb8b93f06ac2b95abe
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-29 02:31:49 +0600

    Commit Thu 08/29/2024  2:31:49.47 11047

--- resources/views/shop/singleproduct.blade.php
+++ resources/views/shop/singleproduct.blade.php
@@ -161,7 +161,7 @@
                             </span>
                           </li>
                           <li><b>Shipping</b> <span>01 day shipping. <samp>Free pickup today</samp></span></li>
-                          <li><b>Weight</b> <span>0.5 kg</span></li>
+                          <li><b>Weight</b> <span>{{ $product->shorttext }}</span></li>
                           <li><b>Share on</b>
                               <div class="share">
                                   <a href="https://www.facebook.com/sharer/sharer.php?u={{ Request::url() }}" onclick="window.open(this.href,'newwindow', 'width=500,height=400'); return false;"><i class="fa fa-facebook"></i></a>
