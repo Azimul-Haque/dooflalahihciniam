bb561d79585bd13689702bd5477da8771befa1e2
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-29 03:34:00 +0600

    Commit Thu 08/29/2024  3:34:00.23 4872

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -90,10 +90,10 @@ class ProductController extends Controller
                         ->orWhere("shorttext", 'LIKE', '%' . $search_param . '%')
                         ->orWhere("price", 'LIKE', '%' . $search_param . '%')
                         ->orWhere("oldprice", 'LIKE', '%' . $search_param . '%')
-                        ->orWhereHas('categories', function ($q) use ($search_param) {
+                        ->WhereHas('categories', function ($q) use ($search_param) {
                             $q->where('name', 'like', '%' . $search_param . '%');
                         })
-                        ->orWhereHas('subcategories', function ($q) use ($search_param) {
+                        ->WhereHas('subcategories', function ($q) use ($search_param) {
                             $q->where('name', 'like', '%' . $search_param . '%');
                         })
                         ->orderBy('id', 'desc')
