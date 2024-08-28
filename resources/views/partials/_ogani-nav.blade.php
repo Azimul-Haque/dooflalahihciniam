f62e177ae3e80177b9f76f182e4411ee0775a2b4
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-29 03:05:39 +0600

    Commit Thu 08/29/2024  3:05:39.30 840

--- resources/views/partials/_ogani-nav.blade.php
+++ resources/views/partials/_ogani-nav.blade.php
@@ -343,7 +343,7 @@
                 <div class="col-lg-9">
                     <div class="hero__search">
                         <div class="hero__search__form">
-                            <form action="#!">
+                            <form onsubmit="myFunction(); return false;">
                                 {{-- <div class="hero__search__categories">
                                     All Categories
                                     <span class="arrow_carrot-down"></span>
