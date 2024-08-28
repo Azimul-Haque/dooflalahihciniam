064ae724fc55f48fcfbfa2898d2df278f7cc0cc9
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-29 02:50:34 +0600

    Commit Thu 08/29/2024  2:50:34.23 15367

--- resources/views/partials/_ogani-nav.blade.php
+++ resources/views/partials/_ogani-nav.blade.php
@@ -348,7 +348,7 @@
                                     All Categories
                                     <span class="arrow_carrot-down"></span>
                                 </div> --}}
-                                <input type="text" placeholder="What do yo u need?">
+                                {{-- <input type="text" placeholder="What do yo u need?"> --}}
                                 <input type="text" class="form-control" name="searchParam" id="searchParam" placeholder="What do yo u need?" @if(!empty($searchparam)) value="{{ $searchparam }}" @endif>
                                 <button class="site-btn">SEARCH</button>
                             </form>
