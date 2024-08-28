86ce2f874b59aa4ef9dbc84224f9d419cdc082b3
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-29 02:51:26 +0600

    Commit Thu 08/29/2024  2:51:26.28 20106

--- resources/views/partials/_ogani-nav.blade.php
+++ resources/views/partials/_ogani-nav.blade.php
@@ -350,7 +350,7 @@
                                 </div> --}}
                                 {{-- <input type="text" placeholder="What do yo u need?"> --}}
                                 <input type="text" class="form-control" name="searchParam" id="searchParam" placeholder="What do yo u need?" @if(!empty($searchparam)) value="{{ $searchparam }}" @endif>
-                                <button class="site-btn" id="searchBtn">SEARCH</button>
+                                <button class="site-btn" id="searchBtn"><span class="glyphicon glyphicon-search"></span> SEARCH</button>
                             </form>
                         </div>
                         <div class="hero__search__phone call-to-this-number">
