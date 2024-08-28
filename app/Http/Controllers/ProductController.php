f70d3a358e0c299850445b6cf46781c8af93fdff
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-29 04:14:11 +0600

    Commit Thu 08/29/2024  4:14:11.24 3763

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -75,7 +75,7 @@ class ProductController extends Controller
                   ->withLatestproducts($latestproducts);
     }
 
-    public function searchProducts($search_param) 
+    public function searchProducts($request, $search_param) 
     {
 
       // $products = Product::where("title", 'LIKE', '%' . $search_param . '%')
