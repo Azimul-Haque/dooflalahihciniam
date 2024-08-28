2a4e3f5831ad7f69793cd6c3365f5cd2e7619508
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-29 02:54:09 +0600

    Commit Thu 08/29/2024  2:54:09.29 10470

--- resources/views/partials/_ogani-nav.blade.php
+++ resources/views/partials/_ogani-nav.blade.php
@@ -425,3 +425,30 @@
             </div>
         </div>
     </section>
+
+    <script type="text/javascript">
+        $('#searchBtn').click(function() {
+    var searchParam = $('#searchParam').val();
+    if(isEmptyOrSpaces(searchParam)) {
+      if($(window).width() > 768) {
+        toastr.warning('Write something on search box!', 'WARNING').css('width', '400px');
+      } else {
+        toastr.warning('Write something on search box!', 'WARNING').css('width', ($(window).width()-25)+'px');
+      }
+    } else {
+      window.location.href = '/warehouse/products/search/' + searchParam;
+    }
+  })
+  // on enter search
+  var input = document.getElementById("searchParam");
+  input.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+     event.preventDefault();
+     document.getElementById("searchBtn").click();
+    }
+  });
+  // on enter search
+  function isEmptyOrSpaces(str){
+      return str === null || str.match(/^ *$/) !== null;
+  }
+    </script>
